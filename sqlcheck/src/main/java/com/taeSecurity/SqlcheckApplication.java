7766b6b105de889b86ac5659b055f358667b5114
TaeYoungHum <gsdj1990123@naver.com>
2023-08-08 14:36:54 +0900

    f
    f

---
+++ sqlcheck/src/main/java/com/taeSecurity/SqlcheckApplication.java
@@ -0,0 +1,13 @@
+package com.taeSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlcheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlcheckApplication.class, args);
+	}
+
+}
---
+++ sqlcheck/src/test/java/com/taeSecurity/SqlcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taeSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlcheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
